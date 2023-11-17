41455ab7e7ded36a7781d31bc0e0a1d167d588ee
Tchongas <66533806+Tchongas@users.noreply.github.com>
2023-11-16 22:07:35 -0300

    hotfix

--- repository.js
+++ repository.js
@@ -7,23 +7,23 @@ var Escola = {
       return db.query("SELECT aluno.*, student_class.class_id FROM aluno LEFT JOIN student_class ON aluno.id = student_class.student_id ORDER BY aluno.id", null, callback);
    },
    //insere as informacoes do HTML na table Aluno, acha a sala que tem os mesmos horarios que especificou
-   addAluno: function (venda, callback) {
+   addAluno: function (input, callback) {
       db.query("INSERT INTO aluno (id, nome, email) VALUES (?,?,?)", 
          [
-            venda.id, 
-            venda.aluno, 
-            venda.email
+            input.id, 
+            input.aluno, 
+            input.email
          ], function(err) {
             if (err) {
                callback(err, null);
             } else {
-               db.query("SELECT id FROM class WHERE diaSemana = ? AND turno = ?", [venda.diaSemana, venda.turno], function(err, rows) { 
+               db.query("SELECT id FROM class WHERE diaSemana = ? AND turno = ?", [input.diaSemana, input.turno], function(err, rows) { 
                   if (err) {
                      callback(err, null);
                   } else {
                      if (rows.length > 0) {
                         const classId = rows[0].id;
-                        db.query("INSERT INTO student_class (student_id, class_id) VALUES (?, ?)", [venda.id, classId], callback);
+                        db.query("INSERT INTO student_class (student_id, class_id) VALUES (?, ?)", [input.id, classId], callback);
                      } else {
                         callback("No class found with the given criteria", null);
                      }
@@ -34,13 +34,13 @@ var Escola = {
    },
 
    //nao fiz ainda
-   updateAlunos: function (venda, callback) {
+   updateAlunos: function (input, callback) {
 
       return db.query("UPDATE alunos SET aluno=?, disciplina=?, diaSemana=?, turno=?, WHERE id=?", 
       [
-         venda.produto, venda.vendedor, venda.cliente, venda.valor,
-         venda.data.getFullYear()+'/'+(venda.data.getMonth()+1)+'/'+venda.data.getDate(), 
-         venda.id
+         input.produto, input.vendedor, input.cliente, input.valor,
+         input.data.getFullYear()+'/'+(input.data.getMonth()+1)+'/'+input.data.getDate(), 
+         input.id
       ], callback);
    }
 
